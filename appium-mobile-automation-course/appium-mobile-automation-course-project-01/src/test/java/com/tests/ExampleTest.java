631bf3731f2f9833910e0f53c903a15369626057
Harshal Chavan <harshalchavan132@outlook.com>
2021-05-03 20:22:01 +0530

    project setup

--- appium-mobile-automation-course/appium-mobile-automation-course-project-01/src/test/java/com/tests/ExampleTest.java
+++ appium-mobile-automation-course/appium-mobile-automation-course-project-01/src/test/java/com/tests/ExampleTest.java
@@ -1,10 +1,21 @@
 package com.tests;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ExampleTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+
+		
+		
+	}
+	
+	public void addStrings(List<? extends String> list) {
+		list.add("foo");
 	}
 
 }
